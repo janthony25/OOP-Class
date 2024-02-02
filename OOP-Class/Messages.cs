34f4352b17f0be526fd05cad608750998f3af592
janthony25 <junanthony.business@gmail.com>
2024-02-02 22:59:57 +1300

    Add project files.

---
+++ OOP-Class/Messages.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Class
+{
+    public class Messages
+    {
+        public static void Hello()
+        {
+            Console.WriteLine("Hello, How are you?");
+        }
+
+        public static void Waiting()
+        {
+            Console.WriteLine("I am just waiting...");
+        }
+
+        public static void Bye()
+        {
+            Console.WriteLine("It was nice to meet you!");
+        }
+    }
+}
---
+++ OOP-Class/Program.cs
@@ -0,0 +1,24 @@
+﻿using OOP_Class;
+using System;
+
+namespace OOP
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // class = A bundle of related code.
+            //         Can be used as a blueprint to create objects (OOP)
+
+            Messages.Hello();
+            Messages.Waiting();
+            Messages.Bye();
+
+
+
+            Console.ReadKey();
+        }
+    }
+
+   
+}
